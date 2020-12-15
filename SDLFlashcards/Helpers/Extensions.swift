d5deb91c987f786c8e98d7e27359a2d6f6b2c532
lapinskijw <jlapinski.dev@gmail.com>
2020-12-14 19:34:46 -0500

    Updated UI and study mode flow. Added new colors.

---
+++ SDLFlashcards/Helpers/Extensions.swift
@@ -0,0 +1,18 @@
+//
+//  Extensions.swift
+//  SDLFlashcards
+//
+//  Created by James Lapinski on 12/14/20.
+//  Copyright © 2020 James Lapinski. All rights reserved.
+//
+
+import Foundation
+import SwiftUI
+
+extension Color {
+    static let lightRed = Color("lightRed")
+    static let darkRed = Color("darkRed")
+    
+    static let lightGreen = Color("lightGreen")
+    static let darkGreen = Color("darkGreen")
+}
--- SDLFlashcards/Views/StudyView.swift
+++ SDLFlashcards/Views/StudyView.swift
@@ -11,22 +11,57 @@ import SwiftUI
 struct StudyView: View {
     @State var flipped = false
     var flashcardSet: FlashcardSet
-    var cardIndex = 0
-
+    @State var cardIndex = 0
+    
     var body: some View {
-        GeometryReader { geometry in
-            ScrollView(.vertical, showsIndicators: false) {
-                VStack(alignment: .leading, spacing: 10) {
-                    ForEach(self.flashcardSet.cards) { flashcard in
-                        FlippinRectangle(answerText: flashcard.answer, questionText: flashcard.question)
+        VStack(alignment: .center, spacing: 10) {
+            FlippinRectangle(answerText: flashcardSet.cards[cardIndex].answer, questionText: flashcardSet.cards[cardIndex].question)
+            HStack(alignment: .center, spacing: nil) {
+                Button(action: {
+                    if cardIndex > 0 {
+                        cardIndex -= 1
                     }
+                }) {
+                    Text("Previous")
+                        .foregroundColor(Color.white)
+                        .fontWeight(.bold)
+                        .font(.body)
                 }
+                .buttonStyle(GradienButtonStyle(colors: [.darkRed, .lightRed]))
+                Spacer()
+                Button(action: {
+                    if cardIndex < flashcardSet.cards.count - 1 {
+                        cardIndex += 1
+                    }
+                }) {
+                    Text("Next Card")
+                        .foregroundColor(Color.white)
+                        .fontWeight(.bold)
+                        .font(.body)
+                }
+                .buttonStyle(GradienButtonStyle(colors: [.darkGreen, .lightGreen]))
             }
-            .navigationBarTitle(Text(self.flashcardSet.name), displayMode: .inline)
-            .padding()
-            .frame(width: geometry.size.width)
-            .cornerRadius(8)
+            .padding([.horizontal], 10)
         }
+    }
+}
+
+struct GradienButtonStyle: ButtonStyle {
+    var colors: [Color]
+    
+    init(colors: [Color]) {
+        self.colors = colors
+    }
+    
+    func makeBody(configuration: Configuration) -> some View {
+        configuration.label
+            .frame(minWidth: 0, maxWidth: .infinity)
+            .padding()
+            .foregroundColor(.white)
+            .background(LinearGradient(gradient: Gradient(colors: colors), startPoint: .leading, endPoint: .trailing))
+            .cornerRadius(40)
+            .padding(.horizontal, 20)
+            .scaleEffect(configuration.isPressed ? 0.95 : 1.0)
     }
 }
 
@@ -34,7 +69,7 @@ struct FlippinRectangle: View {
     var answerText: String
     var questionText: String
     @State var flipped = false
-
+    
     var body: some View {
         HStack {
             Button(action: {
@@ -54,10 +89,13 @@ struct FlippinRectangle: View {
                         .foregroundColor(.white)
                 }
             }
-            .frame(minWidth: 0, maxWidth: .infinity, minHeight: 300)
+            .frame(minWidth: 0, maxWidth: .infinity, maxHeight: 300)
             .background(self.flipped ? Color.blue : Color.green)
             .rotation3DEffect(self.flipped ? Angle(degrees: 180) : Angle(degrees: 0), axis: (x:CGFloat(0), y: CGFloat(10), z: CGFloat(0)))
             .animation(.default)
+            .cornerRadius(8)
+            .padding([.horizontal], 10)
         }
+        .padding([.horizontal], 20)
     }
 }
